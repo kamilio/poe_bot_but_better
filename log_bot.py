7fec77599a56cb4ba87b879d6755629e799ad9c1
Kamil Jopek <kjopek@quora.com>
2025-01-03 13:39:37 -0600

    Cleanup the log bot

--- log_bot.py
+++ log_bot.py
@@ -18,27 +18,11 @@ pformat = PrettyFormat(width=85)
 
 @poe_bot_but_better
 class LogBot:
-    async def get_response(
-        self, messages: List[fp.ProtocolMessage]
-    ) -> AsyncIterable[fp.PartialResponse]:
-        yield "```python\n" + pformat(messages) + "\n```"
+    def get_response(self, messages: List[fp.ProtocolMessage]):
+        return "```python\n" + pformat(messages) + "\n```"
 
     def get_settings(self) -> fp.SettingsResponse:
         return fp.SettingsResponse(
-            allow_attachments=True, enable_image_comprehension=True
+            allow_attachments=True, 
+            enable_image_comprehension=True
         )
-
-
-REQUIREMENTS = ["fastapi-poe==0.0.48", "devtools==0.12.2"]
-image = Image.debian_slim().pip_install(*REQUIREMENTS)
-app = App("log-bot-poe")
-
-
-@app.function(image=image)
-@asgi_app()
-def fastapi_app():
-    bot = LogBot()
-    # see https://creator.poe.com/docs/quick-start#configuring-the-access-credentials
-    # app = fp.make_app(bot, access_key=<YOUR_ACCESS_KEY>, bot_name=<YOUR_BOT_NAME>)
-    app = fp.make_app(bot, allow_without_key=True)
-    return app
