50a174be23a750e1cbad638627522eff4271897d
Kamil Jopek <kjopek@quora.com>
2025-01-03 13:37:19 -0600

    Add support for suggested_replies

--- poe_bot_but_better/test.py
+++ poe_bot_but_better/test.py
@@ -1,10 +1,42 @@
 import pytest
 import fastapi_poe as fp
-from typing import Any, AsyncGenerator, List, Optional
+from typing import Any, AsyncGenerator, List, Optional, Union
 from dataclasses import dataclass, field
 import uuid
+
+import sse_starlette
 from poe_bot_but_better import normalize_request, solve_dependencies
 from unittest.mock import AsyncMock, MagicMock
+
+class ResponseString(str):
+    def __new__(cls, content):
+        instance = super().__new__(cls, content)
+        return instance
+    
+    def __init__(self, content):
+        super().__init__()
+        self.suggested_replies = []
+        self.versions = []
+        self.events = []
+    
+    def append_suggested_reply(self, suggested_reply: str):
+        self.suggested_replies.append(suggested_reply)
+
+    def append_event(self, event: sse_starlette.sse.ServerSentEvent):
+        self.events.append(event)
+    
+    def replacemenet(self, new_content: str):
+        new_string = ResponseString(new_content)
+        new_string.versions.append(self)
+        new_string.suggested_replies = self.suggested_replies
+        return new_string
+    
+    def append(self, new_content: str):
+        new_string = ResponseString(self + new_content)
+        new_string.suggested_replies = self.suggested_replies
+        new_string.versions = self.versions
+        return new_string
+        
 
 mock_query_request = fp.QueryRequest(
     query=[],
@@ -16,14 +48,41 @@ mock_query_request = fp.QueryRequest(
     access_key="test_key"
 )
 
+class AsyncQueryMock(AsyncMock):
+    def __init__(self, bot_name: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bot_name = bot_name
+
+    def inspect_calls(self):
+        print(f"Mock: {self.bot_name}")
+        for i, call in enumerate(self.call_args_list):
+            print(f"Call {i}: args={call.args}, kwargs={call.kwargs}")
+
+    def assert_called_with_content(self, content: str):
+        call_requests = [normalize_request(mock_query_request, call.args[0]) for call in self.call_args_list]
+        for call_args in call_requests:
+            print("yo", call_args.query)
+            for message in call_args.query:
+                if message.content == content:
+                    return
+        output = [call_args.query[0].content for call_args in call_requests]
+        new_line = "\n"
+        raise AssertionError(f"Expected call with content '{content}' not found. Found: {new_line.join(output)}")
+
+        
+
 class BotTestHelper:
     def __init__(self):
         self.mocked_bots = {}
         self.responses = {}
 
-    def mock_bot(self, bot_name: str, responses: List[str]):
+    def mock_bot(self, bot_name: str, responses: Union[str, List[str]]):
+        if isinstance(responses, str):
+            responses = [responses]
+
         self.responses[bot_name] = responses
-        mock = AsyncMock()
+        mock = AsyncQueryMock(bot_name=bot_name)
+
         self.mocked_bots[bot_name] = mock
         return mock
 
@@ -35,13 +94,13 @@ class BotTestHelper:
 
     async def _get_final_response(self, request_or_message, bot_name: str, **kwargs) -> str:
         if bot_name not in self.responses:
-            raise ValueError(f"Bot {bot_name} not mocked")
+            raise ValueError(f"Bot {bot_name} not mocked. Use bot_helper.mock_bot(\"{bot_name}\", \"response\")")
         self.mocked_bots[bot_name](request_or_message)
         return "".join(self.responses[bot_name])
 
     async def _stream_request(self, request_or_message, bot_name: str, **kwargs) -> AsyncGenerator[fp.PartialResponse, None]:
         if bot_name not in self.responses:
-            raise ValueError(f"Bot {bot_name} not mocked")
+            raise ValueError(f"Bot {bot_name} not mocked. Use bot_helper.mock_bot(\"{bot_name}\", \"response\")")
         self.mocked_bots[bot_name](request_or_message)
         async for response in self._create_response_generator(self.responses[bot_name]):
             yield response
@@ -57,12 +116,22 @@ class BotTestHelper:
             "stream_request": self._stream_request,
         }
 
-        response_parts = []
+        response = ResponseString("")
         async for part in bot.get_response(request):
             if isinstance(part, fp.PartialResponse):
-                print(part.text)
-                response_parts.append(part.text)
-        return "".join(response_parts)
+                if part.is_replace_response:
+                    response = response.replacemenet(part.text)
+                elif part.is_suggested_reply:
+                    response.append_suggested_reply(part.text)
+                else:
+                    response = response.append(part.text)
+            # Not sure how is this used
+            elif isinstance(part, sse_starlette.sse.ServerSentEvent):
+                response.append_event(part)
+            else:
+                raise ValueError(f"Unexpected response type: {part}")
+            
+        return response
     
     async def get_settings(self, bot_class) -> fp.SettingsResponse:
         bot_name = "TestBot"
@@ -73,3 +142,9 @@ class BotTestHelper:
 @pytest.fixture
 def bot_helper():
     return BotTestHelper()
+
+def bot_helper_wrong_usage(*args, **kwargs):
+    raise ValueError("Use bot_helper fixture in your test: async def test_something(bot_helper): ...")
+
+bot_helper.mock_bot = bot_helper_wrong_usage
+bot_helper.send_message = bot_helper_wrong_usage
