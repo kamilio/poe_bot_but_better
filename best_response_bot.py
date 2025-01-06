67e78c9ae73f15743b55485ef573dbf246df65da
Kamil Jopek <kjopek@quora.com>
2025-01-06 13:42:54 -0600

    Add new bot - pick best response

---
+++ best_response_bot.py
@@ -0,0 +1,50 @@
+import asyncio
+from dataclasses import dataclass, field
+from typing import Annotated, Any
+from poe_bot_but_better import poe_bot_but_better
+from poe_bot_but_better.dependency_injection import Depends
+import json
+
+@dataclass
+class Config:
+    decision_bot: str = "Claude-3-Haiku"
+    bots: list[str] = field(default_factory=lambda: ["Claude-3-Haiku", "GPT-3.5-Turbo"])
+
+def create_get_final_response_tuple(get_final_response):
+    async def get_final_response_tuple(request, bot_name) -> tuple[str, str]:
+        try:
+            response = await get_final_response(request, bot_name)
+            return bot_name, response
+        except Exception as e:
+            print(f"Error with bot {bot_name}: {e}")
+            return bot_name, ""
+
+    return get_final_response_tuple
+
+
+@poe_bot_but_better
+class BestResponseBot:
+    async def get_response(
+            self, 
+            request, 
+            get_final_response, 
+            get_final_response_tuple: Annotated[Any, Depends(create_get_final_response_tuple)],
+            config: Annotated[Config, Depends()]
+        ):
+        responses = dict(await asyncio.gather(*(get_final_response_tuple(request, bot_name) for bot_name in config.bots)))
+        prompt = "Which response is best? Output only the key from the json. Nothing else is permitted. \n\n"
+        best_key = await get_final_response(prompt+json.dumps(responses, indent=4), config.decision_bot)
+        
+        try:
+            return responses[best_key]
+        except KeyError:
+            print("Key not found, returning first response. Key:", best_key)
+            return responses[config.bots[0]]
+    
+    def get_settings(self, config: Annotated[Config, Depends()]):
+        def count_strings(string_list):
+            return {item: string_list.count(item) for item in set(string_list)}
+        
+        return {
+            "server_bot_dependencies": count_strings(config.bots+[config.decision_bot])
+        }
---
+++ test_best_response_bot.py
@@ -0,0 +1,38 @@
+import pytest
+from poe_bot_but_better.test import bot_helper, BotTestHelper
+from best_response_bot import BestResponseBot, Config
+
+@pytest.fixture
+def config():
+    return Config(bots=["Assistant", "GPT-3.5-Turbo", "Claude-3-Haiku"], decision_bot="Claude-3-Haiku")
+
+@pytest.mark.asyncio
+async def test_best_response_bot(bot_helper: BotTestHelper, config: Config):
+    bot_helper.mock_bot("GPT-3.5-Turbo", "Hi!")
+    bot_helper.mock_bot("Assistant", "Yo!")
+    bot_helper.mock_bot("Claude-3-Haiku", "GPT-3.5-Turbo")
+    response = await bot_helper.send_message(BestResponseBot, "Hello", dependency_injection_context_override={"config": config})
+    assert response == "Hi!"
+
+@pytest.mark.asyncio
+async def test_best_response_wrong_decision(bot_helper: BotTestHelper, config: Config):
+    bot_helper.mock_bot("GPT-3.5-Turbo", "Hi!")
+    bot_helper.mock_bot("Assistant", "Yo!")
+    bot_helper.mock_bot("Claude-3-Haiku", "NOT_EXISTENT_BOT")
+    response = await bot_helper.send_message(BestResponseBot, "Hello", dependency_injection_context_override={"config": config})
+    assert response == "Yo!"
+
+# some bot errors out
+@pytest.mark.asyncio
+async def test_best_response_bot_error(bot_helper: BotTestHelper, config: Config):
+    bot_helper.mock_bot("GPT-3.5-Turbo", "Hi!")
+    bot_helper.mock_bot("Assistant", lambda *args, **kwargs: 1/0)
+    bot_helper.mock_bot("Claude-3-Haiku", "GPT-3.5-Turbo")
+    response = await bot_helper.send_message(BestResponseBot, "Hello", dependency_injection_context_override={"config": config})
+    assert response == "Hi!"
+    
+
+@pytest.mark.asyncio
+async def test_settings(bot_helper: BotTestHelper, config: Config):
+    settings = await bot_helper.get_settings(BestResponseBot, dependency_injection_context_override={"config": config})
+    assert settings.server_bot_dependencies == {"Assistant": 1, "GPT-3.5-Turbo": 1, "Claude-3-Haiku": 2}
