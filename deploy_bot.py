b3a678eb6e7da508e290edbb55653b8a3b3d7eb3
Kamil Jopek <kjopek@quora.com>
2025-01-06 16:27:23 -0600

    Update deploy script

--- deploy_bot.py
+++ deploy_bot.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional
 from modal import App, Image, asgi_app, Secret
-from prompt_bot import PromptBot
+from best_response_bot import BestResponseBot
 import fastapi_poe as fp
 
 REQUIREMENTS = ["fastapi-poe==0.0.48"]
@@ -10,14 +10,15 @@ app = App("poe-bot-poe")
 
 # 1 Deploy the bot - `modal server deploy_bot.py` (copy the deployment URL - ends with .modal.run)
 # 2 Create bot on Poe - https://poe.com/create_bot?server=1 (copy access key)
-access_key = ""
+access_token = ""
 # 3 Set the name of the bot 
 bot_name = ""
+
 # 4 (optional) use Modal's secret https://modal.com/secrets/ and Secret.from_name
-@app.function(image=image, secrets=[Secret.from_dict({"POE_BOT_ACCESS_KEY": access_key})])
+@app.function(image=image, secrets=[Secret.from_dict({"POE_BOT_ACCESS_KEY": access_token})])
 @asgi_app()
 def fastapi_app():
-    bot = PromptBot(access_key=access_key, bot_name=bot_name)
     access_key = os.environ.get("POE_BOT_ACCESS_KEY")
+    bot = BestResponseBot(access_key=access_key, bot_name=bot_name)
     app = fp.make_app(bot, allow_without_key=(not access_key))
     return app
